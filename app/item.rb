4d1d25b8310a80e170609cedc94ddef27d7079c1
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-08-04 14:02:28 -0400

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,26 @@
 class Item
+  @@all = []
   attr_accessor :name, :price
 
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.item_price_by_name(search)
+    found_item = Item.all.find{|item| item.name == search}
+    #binding.pry
+    if found_item 
+      return found_item.price
+    else
+      return false
+    end
+  end
+
+
 end
